f1bef89064ce235a00d226aad943a094ed28872e
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-05 17:49:38 +0100

    Fixing amount bug

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -2,7 +2,13 @@ class SplashController < ApplicationController
   def index
     return unless current_user.present?
 
-    @categories = current_user.groups.all
-    @amount = current_user.categories.all.sum(:amount)
+    @categories = current_user.groups.includes(:group_categories)
+    
+     
+    @totals = @categories.map do |catagory|
+      catagory.group_categories.reduce(0) { |sum, num| sum + num.category.amount }
+    end
+
+   
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :groups
   has_many :categories
