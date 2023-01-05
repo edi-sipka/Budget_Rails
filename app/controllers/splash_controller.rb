a9e085cb6e1993a582ef5a091ba6540e73228291
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-05 17:51:25 +0100

    Fix linters

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -3,12 +3,10 @@ class SplashController < ApplicationController
     return unless current_user.present?
 
     @categories = current_user.groups.includes(:group_categories)
-    
-     
+
+
     @totals = @categories.map do |catagory|
       catagory.group_categories.reduce(0) { |sum, num| sum + num.category.amount }
     end
-
-   
   end
 end
