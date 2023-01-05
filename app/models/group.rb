d12456a4581b15d60901c567442743fd883e79bd
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-05 18:14:07 +0100

    Fix linters

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,12 +2,6 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :categories, dependent: :destroy
   has_many :group_categories, dependent: :destroy
-
   validates :name, presence: true
   validates :icon, presence: true
-
-
-  def total_amount
-    categories.sum(:amount)
-  end
 end
